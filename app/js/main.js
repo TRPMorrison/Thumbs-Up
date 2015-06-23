f302cff346e76fc8e006cc0a337bc518e8a6f077
Trini Morrison <TRPMorrison@gmail.com>
2015-06-23 14:50:22 -0400

    INITIAL COMMIT

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+;(function (){
+
+  'use strict';
+
+  angular.module('ThumbModule',[])
+
+    .controller('ThumbController', ['$scope',
+      function ($scope) {
+
+        $scope.clickThumb = [];
+      }])
+
+
+
+
+  });
+}());
